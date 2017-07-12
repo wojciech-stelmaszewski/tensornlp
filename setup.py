374f834e1f11a75531709709361a6537a0d5e83b
Wojciech Stelmaszewski <stelmaszewskiw@gmail.com>
2017-07-12 23:20:33 +0200

    tensornlp module added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tensornlp',
+      version='0.1',
+      description='NLP algorithms implemented using linear algebra interpretations.',
+      url='https://github.com/wojciech-stelmaszewski/tensornlp',
+      author='Wojciech Stelmaszewski',
+      author_email='stelmaszewskiw@gmail.com',
+      license='MIT',
+      packages=['tensornlp'],
+      zip_safe=False)
